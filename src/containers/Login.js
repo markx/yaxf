4513c2d8d60eb63ebc031502301c1d518b988a09
Ke Xu <kxu000@gmail.com>
2016-05-23 22:39:17 -0400

    make login page fill window size

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -22,16 +22,25 @@ module.exports = React.createClass({
 
     render () {
         return (
-            <webview ref="webview"
+            <div style={{
+                width: '100vw',
+                height: '100vh'
+
+            }}
+        >
+            <webview
+                ref="webview"
                 src="http://ui.ptlogin2.qq.com/cgi-bin/login?uin=&appid=567008010&f_url=loginerroralert&hide_title_bar=1&style=1&s_url=http%3A//lixian.qq.com/main.html&lang=0&enable_qlogin=1&css=http%3A//imgcache.qq.com/ptcss/r1/txyjy/567008010/login_mode_new.css"
+                autosize="on"
                 style= {{
-                    display: 'inline-flex',
                     width: '100%',
                     height: '100%'
                 }}
-            ></webview>
-            // <webview ref="webview" src="http://lixian.qq.com/login.html?opt=open_login"  ></webview>
-            // <webview ref="webview" src="http://ui.ptlogin2.qq.com/cgi-bin/login?appid=567008010&s_url=http%3A//lixian.qq.com/main.html&lang=0&enable_qlogin=1&style=1" ></webview>
-        );
+                    />
+                </div>
+
+                // <webview ref="webview" src="http://lixian.qq.com/login.html?opt=open_login"  ></webview>
+                // <webview ref="webview" src="http://ui.ptlogin2.qq.com/cgi-bin/login?appid=567008010&s_url=http%3A//lixian.qq.com/main.html&lang=0&enable_qlogin=1&style=1" ></webview>
+        )
     }
 })
